6e13f20a5bf05fd1f11d9de9c5730ca0c3bda27c
Marcus <marcus_oliveira1@protonmail.com>
2022-03-09 22:03:25 -0300

    Aula 5

---
+++ aula5/aula5.py
@@ -0,0 +1,19 @@
+"""
++, -, * , /, //, **, %, ()
+"""
+print('Multiplicação =', 10 * 10)
+print('Adição =', 10 + 10)
+print('Subtração =', 10 - 5)
+print('Divisão =', 10 / 2)
+
+print('Multiplicação =', 10 * 'A')
+# Concatenação
+print('Adição =', '5' + '5')
+print('Luiz' + ' ' + 'Otávio e ele tem ' + str(32) + ' anos')
+
+print(10.5 // 3)
+print(2 ** 10)
+print(10 % 3)
+#Precedencia
+print(5+2*10)
+print((5+2)*10)
