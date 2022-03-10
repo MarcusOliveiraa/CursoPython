3f2b163b8b95a3d2bf47bc097c0f127418a25f0c
Marcus <marcus_oliveira1@protonmail.com>
2022-03-09 21:52:33 -0300

    Aula 4

---
+++ aula4/aula4.py
@@ -0,0 +1,27 @@
+"""
+Tipos de dados
+str - string - texto 'Assim' "Assim
+int - inteiro - 123456 0 -10 -20 -50 1500
+flat - real/ponto flutuante - 10.5 1.5 -10.10 -50.93 0.0
+bool - booleano/logico - true/false - 10 == 10
+"""
+
+print('Luiz', type('Luiz'))
+print(10, type(10))
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
+
+print('Luiz', type('Luiz'), bool('Luiz'))
+print('10', type('10'), type(int(10)))
+
+# String: nome
+print('Marcus Oliveira', type('Marcus Oliveira'))
+
+# Idade: int
+print('25', type('25'))
+
+# Altura: float
+print('1.77', type('1.77'))
+
+# è maior de idade 10 > 20
+print('25 > 18', type('25 > 18 '))
