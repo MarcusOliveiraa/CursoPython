f493382c79d8dbc9787620d8b2fdf2e1cca7c4f8
Marcus <marcus_oliveira1@protonmail.com>
2022-03-09 21:24:16 -0300

    Até a aula2

---
+++ aula1.py
@@ -0,0 +1,15 @@
+"""
+print('Linha 1')
+print('Linha 2')
+# Falando do que se trata essa linha
+print('Linha 3')
+"""
+
+'''
+print('Linha 4')  # Isso é um comentário
+'''
+
+print('Linha 5')  # mais um
+print('Linha 6')
+
+# Final
---
+++ aula2/aula2.py
@@ -0,0 +1,17 @@
+# print(123456)
+
+# print('Luiz', 'Otávio', 'Outra Coisa')
+
+print('Luiz', 'Otávio', sep='-', end='%%%%%%')
+
+print('João', 'e', 'Maria', sep='-', end='')
+
+# Python diferencia letras maiusculas e minusculas
+# Print('Executar')
+
+"""
+824.176.070-18
+"""
+
+print('824', '176', '070', sep='.', end='_')
+print('18')
